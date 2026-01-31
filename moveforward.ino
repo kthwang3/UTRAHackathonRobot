502869db568aa79aceafa41802b8b77516839431
kthwang3 <kthwang3@gmail.com>
2026-01-31 17:42:38 -0500

    Update moveforward.ino

--- moveforward.ino
+++ moveforward.ino
@@ -1,55 +1,9 @@
-// =====================
-// MOTOR PIN DEFINITIONS
-// =====================
-
-// Left motor
-#define ENA 9
-#define IN1 8
-#define IN2 7
-
-// Right motor
-#define ENB 4
-#define IN3 6
-#define IN4 5
-
-// Speed values (0–255)
-int motorSpeed = 255;
-
-void setup() {
-  // Set all motor pins as outputs
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-
-  pinMode(ENB, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-
-  // Stop motors at start
-  stopMotors();
-}
-
-void loop() {
-  // Move forward
-  moveForward(motorSpeed);
-  delay(2000);
-
-  // Stop
-  stopMotors();
-  delay(1000);
-
-}
-
-// =====================
-// MOTOR FUNCTIONS
-// =====================
-
 void moveForward(int speed) {
-  // Left motor forward
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
+  // Left motor forward (FLIPPED)
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
 
-  // Right motor forward
+  // Right motor forward (unchanged)
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 
@@ -58,19 +12,14 @@ void moveForward(int speed) {
 }
 
 void moveBackward(int speed) {
-  // Left motor backward
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+  // Left motor backward (FLIPPED)
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 
-  // Right motor backward
+  // Right motor backward (unchanged)
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 
   analogWrite(ENA, speed);
   analogWrite(ENB, speed);
-}
-
-void stopMotors() {
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
 }
